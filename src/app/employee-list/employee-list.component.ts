5b52535df604c272082e8cc659237eb43bcdab7d
Sasikumar-s <sasikumarbe2018@gmail.com>
2022-02-06 11:45:25 +0530

    First upload

---
+++ src/app/employee-list/employee-list.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { Employee } from '../employee';
+import { EmployeeService } from '../employee.service';
+
+@Component({
+  selector: 'app-employee-list',
+  templateUrl: './employee-list.component.html',
+  styleUrls: ['./employee-list.component.css']
+})
+export class EmployeeListComponent implements OnInit {
+  employee: Employee[] =[];
+  constructor(private employeeService:EmployeeService) { }
+
+  ngOnInit(): void {
+   this.getEmployee();
+  }
+  private getEmployee(){
+    this.employeeService.getEmployeeList().subscribe(
+      (success:any)=>{
+        console.log(success);
+      },
+      (failure:any)=>{
+        console.log(failure);
+      },
+    )};
+  
+
+
+}
